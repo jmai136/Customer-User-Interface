eefc1cfe2d030bb28cfa8a670cd78d4bcd1cf66f
jmai136 <jude.mai@student.centralia.edu>
2025-01-31 10:28:19 -0800

    fix: hashes password

--- GourmetShopSolution/GourmetShop.DataAccess/Services/Authentication Subservices/AuthService.cs
+++ GourmetShopSolution/GourmetShop.DataAccess/Services/Authentication Subservices/AuthService.cs
@@ -26,6 +26,8 @@ namespace GourmetShop.DataAccess.Services
         // CHECKME: Do we need a separate email parameter? Because in the form you have username/email as a field, how does the email validator work?
         public int Register(User user, Authentication authentication)
         {
+            authentication.Password = PasswordHasher.HashPassword(authentication.Password);
+
             return _authRepository.Register(user, authentication);
         }
 
