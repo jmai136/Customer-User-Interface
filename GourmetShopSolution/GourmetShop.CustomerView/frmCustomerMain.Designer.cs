09a19985dbccf5bcc21deeca021b31a719231c68
smcrownhart <103695112+smcrownhart@users.noreply.github.com>
2025-02-02 01:59:44 -0800

    forgot to add comments. Check my add to cart functions, especially session data and customerId

--- GourmetShopSolution/GourmetShop.CustomerView/frmCustomerMain.Designer.cs
+++ GourmetShopSolution/GourmetShop.CustomerView/frmCustomerMain.Designer.cs
@@ -30,13 +30,20 @@
         {
             this.btnShoppingCart = new System.Windows.Forms.Button();
             this.dgvCustViewProducts = new System.Windows.Forms.DataGridView();
+            this.cboCustSuppliers = new System.Windows.Forms.ComboBox();
+            this.lblSupplierProducts = new System.Windows.Forms.Label();
+            this.nudQuantity = new System.Windows.Forms.NumericUpDown();
+            this.lblQuantity = new System.Windows.Forms.Label();
+            this.btnAddToCart = new System.Windows.Forms.Button();
+            this.lblAddToCart = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvCustViewProducts)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudQuantity)).BeginInit();
             this.SuspendLayout();
             // 
             // btnShoppingCart
             // 
             this.btnShoppingCart.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnShoppingCart.Location = new System.Drawing.Point(1316, 467);
+            this.btnShoppingCart.Location = new System.Drawing.Point(737, 467);
             this.btnShoppingCart.Name = "btnShoppingCart";
             this.btnShoppingCart.Size = new System.Drawing.Size(191, 32);
             this.btnShoppingCart.TabIndex = 0;
@@ -47,24 +54,92 @@
             // dgvCustViewProducts
             // 
             this.dgvCustViewProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvCustViewProducts.Location = new System.Drawing.Point(77, 63);
+            this.dgvCustViewProducts.Location = new System.Drawing.Point(12, 12);
             this.dgvCustViewProducts.Name = "dgvCustViewProducts";
             this.dgvCustViewProducts.RowHeadersWidth = 51;
             this.dgvCustViewProducts.RowTemplate.Height = 24;
-            this.dgvCustViewProducts.Size = new System.Drawing.Size(1246, 337);
+            this.dgvCustViewProducts.Size = new System.Drawing.Size(497, 487);
             this.dgvCustViewProducts.TabIndex = 1;
+            this.dgvCustViewProducts.DataBindingComplete += new System.Windows.Forms.DataGridViewBindingCompleteEventHandler(this.dgvCustViewProducts_DataBindingComplete);
+            // 
+            // cboCustSuppliers
+            // 
+            this.cboCustSuppliers.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboCustSuppliers.FormattingEnabled = true;
+            this.cboCustSuppliers.Location = new System.Drawing.Point(603, 75);
+            this.cboCustSuppliers.Name = "cboCustSuppliers";
+            this.cboCustSuppliers.Size = new System.Drawing.Size(261, 28);
+            this.cboCustSuppliers.Sorted = true;
+            this.cboCustSuppliers.TabIndex = 2;
+            this.cboCustSuppliers.SelectedIndexChanged += new System.EventHandler(this.cboCustSuppliers_SelectedIndexChanged);
+            // 
+            // lblSupplierProducts
+            // 
+            this.lblSupplierProducts.AutoSize = true;
+            this.lblSupplierProducts.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSupplierProducts.Location = new System.Drawing.Point(597, 36);
+            this.lblSupplierProducts.Name = "lblSupplierProducts";
+            this.lblSupplierProducts.Size = new System.Drawing.Size(266, 25);
+            this.lblSupplierProducts.TabIndex = 3;
+            this.lblSupplierProducts.Text = "Filter Products by Supplier";
+            // 
+            // nudQuantity
+            // 
+            this.nudQuantity.Location = new System.Drawing.Point(687, 226);
+            this.nudQuantity.Name = "nudQuantity";
+            this.nudQuantity.Size = new System.Drawing.Size(120, 22);
+            this.nudQuantity.TabIndex = 4;
+            // 
+            // lblQuantity
+            // 
+            this.lblQuantity.AutoSize = true;
+            this.lblQuantity.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuantity.Location = new System.Drawing.Point(540, 228);
+            this.lblQuantity.Name = "lblQuantity";
+            this.lblQuantity.Size = new System.Drawing.Size(123, 20);
+            this.lblQuantity.TabIndex = 5;
+            this.lblQuantity.Text = "Select Quantity";
+            // 
+            // btnAddToCart
+            // 
+            this.btnAddToCart.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddToCart.Location = new System.Drawing.Point(687, 296);
+            this.btnAddToCart.Name = "btnAddToCart";
+            this.btnAddToCart.Size = new System.Drawing.Size(120, 38);
+            this.btnAddToCart.TabIndex = 6;
+            this.btnAddToCart.Text = "Add to Cart";
+            this.btnAddToCart.UseVisualStyleBackColor = true;
+            this.btnAddToCart.Click += new System.EventHandler(this.btnAddToCart_Click);
+            // 
+            // lblAddToCart
+            // 
+            this.lblAddToCart.AutoSize = true;
+            this.lblAddToCart.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAddToCart.Location = new System.Drawing.Point(703, 352);
+            this.lblAddToCart.Name = "lblAddToCart";
+            this.lblAddToCart.Size = new System.Drawing.Size(0, 20);
+            this.lblAddToCart.TabIndex = 7;
+            this.lblAddToCart.Visible = false;
             // 
             // frmCustomerMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1538, 511);
+            this.ClientSize = new System.Drawing.Size(949, 511);
+            this.Controls.Add(this.lblAddToCart);
+            this.Controls.Add(this.btnAddToCart);
+            this.Controls.Add(this.lblQuantity);
+            this.Controls.Add(this.nudQuantity);
+            this.Controls.Add(this.lblSupplierProducts);
+            this.Controls.Add(this.cboCustSuppliers);
             this.Controls.Add(this.dgvCustViewProducts);
             this.Controls.Add(this.btnShoppingCart);
             this.Name = "frmCustomerMain";
             this.Text = "Customer";
             ((System.ComponentModel.ISupportInitialize)(this.dgvCustViewProducts)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudQuantity)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -72,6 +147,12 @@
 
         private System.Windows.Forms.Button btnShoppingCart;
         private System.Windows.Forms.DataGridView dgvCustViewProducts;
+        private System.Windows.Forms.ComboBox cboCustSuppliers;
+        private System.Windows.Forms.Label lblSupplierProducts;
+        private System.Windows.Forms.NumericUpDown nudQuantity;
+        private System.Windows.Forms.Label lblQuantity;
+        private System.Windows.Forms.Button btnAddToCart;
+        private System.Windows.Forms.Label lblAddToCart;
     }
 }
 
--- GourmetShopSolution/GourmetShop.CustomerView/frmCustomerMain.cs
+++ GourmetShopSolution/GourmetShop.CustomerView/frmCustomerMain.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -17,22 +18,169 @@ namespace GourmetShop.CustomerView
     {
         public static string connectionString = ConfigurationManager.ConnectionStrings["GourmetShopConnectionString"].ConnectionString;
         private ProductRepository productRepository = new ProductRepository(connectionString);
+        private SupplierRepository supplierRepository = new SupplierRepository(connectionString);
         public frmCustomerMain()
         {
             InitializeComponent();
             LoadProducts();
+            dgvCustViewProducts.DataBindingComplete += dgvCustViewProducts_DataBindingComplete;
+            SupplierList();
+            cboCustSuppliers.SelectedIndex = 0;
         }
 
+
+        private void cboCustSuppliers_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (cboCustSuppliers.SelectedIndex == 0)
+                {
+                    LoadProducts();
+                }
+                else if (cboCustSuppliers.SelectedItem is Supplier selectedSupplier)
+                {
+                    string supplierName = selectedSupplier.CompanyName;
+                    LoadProductsBySupplierName(supplierName);
+                }
+
+
+            } catch (Exception ex) 
+            {
+                MessageBox.Show($"Unable to load suppliers or products: {ex.Message}");
+            }
+            
+        }
         private void LoadProducts()
         {
-            List<Product> availableProducts = (List<Product>)productRepository.GetAvailableProductsForAdmin();
-            dgvCustViewProducts.DataSource = availableProducts;
+            try
+            {
+                List<Product> availableProducts = (List<Product>)productRepository.GetAvailableProductsForCust();
+                dgvCustViewProducts.DataSource = availableProducts;
+                //Hide columns that aren't pertinant to the customer
+                //https://learn.microsoft.com/en-us/dotnet/desktop/winforms/controls/how-to-hide-columns-in-the-windows-forms-datagridview-control?view=netframeworkdesktop-4.8
+                dgvCustViewProducts.Columns["SupplierID"].Visible = false;
+                dgvCustViewProducts.Columns["Package"].Visible = false;
+            }catch (Exception ex)
+            {
+                MessageBox.Show($"Unable to load Products: {ex.Message}");
+            }
+            
+            
+            
         }
 
         private void btnShoppingCart_Click(object sender, EventArgs e)
         {
             frmShoppingCart cart = new frmShoppingCart();
             cart.Show();
+        }
+
+
+        //ChatGpt Suggested this event handler to hide the IsDiscontinued Column. Couldn't find a good answer anywhere else.
+        //This column is not pertinant to customers as we already filter out discontinued.
+        private void dgvCustViewProducts_DataBindingComplete(object sender, DataGridViewBindingCompleteEventArgs e)
+        {
+            try
+            {
+                if (dgvCustViewProducts.Columns.Contains("IsDisContinued"))
+                {
+                    dgvCustViewProducts.Columns["IsDiscontinued"].Visible = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"The event handler is not behaving: {ex.Message}");
+            }
+
+        }
+
+        private void SupplierList()
+        {
+            try
+            {
+                string connectionString = ConfigurationManager.ConnectionStrings["GourmetShopConnectionString"].ConnectionString;
+                var supplierRepository = new SupplierRepository(connectionString);
+                var suppliers = supplierRepository.GetAll().ToList();
+
+                suppliers.Insert(0, new Supplier { Id = -1, CompanyName = "-- Select a Supplier --" });
+
+                cboCustSuppliers.DataSource = suppliers;
+                cboCustSuppliers.DisplayMember = "CompanyName";
+                cboCustSuppliers.ValueMember = "Id";
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Unable to load Suppliers: {ex.Message}");
+            }
+
+        }
+
+        private void LoadProductsBySupplierName(string supplierName)
+        {
+            try
+            {
+                string connectionString = ConfigurationManager.ConnectionStrings["GourmetShopConnectionString"].ConnectionString;
+                var productRepository = new ProductRepository(connectionString);
+
+                List<Product> filteredProds = productRepository.GetSupplierByName(supplierName);
+                dgvCustViewProducts.DataSource = filteredProds;
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show($"Unable to filter products by suppliers: {ex.Message}");
+            }
+        }
+        //TODO
+        //Someone smarter than me double check this!!!!
+        private void AddToCart(int customerId, int productId, int quantity)
+        {
+            //_ = SessionData.CurrentCustomerId;
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                using (SqlCommand cmd = new SqlCommand("AddToCart", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@CustomerID", customerId);
+                    cmd.Parameters.AddWithValue("@ProductID", productId);
+                    cmd.Parameters.AddWithValue("@Quantity", quantity);
+                    
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+
+            }catch(Exception ex){
+                lblAddToCart.Text = "Failed to add to cart because: " + ex.Message;
+            }
+        }
+
+        private void btnAddToCart_Click(object sender, EventArgs e)
+        {
+            if(dgvCustViewProducts.SelectedRows.Count == 0)
+            {
+                lblAddToCart.Text = "Select a product to add to cart";
+                return;
+            }
+
+            if(SessionData.CurrentCustomerId <= 0)
+            {
+                lblAddToCart.Text = "Your session has expired. You will need to log in";
+                return;
+            }
+
+            int productId = Convert.ToInt32(dgvCustViewProducts.SelectedRows[0].Cells["Id"].Value);
+            int customerId = SessionData.CurrentCustomerId;
+            int quantiy = (int)nudQuantity.Value;
+
+            if(nudQuantity.Value < 1)
+            {
+                lblAddToCart.Text = "Select a quantity";
+                return;
+            }
+
+            AddToCart(customerId, productId, quantiy);
         }
     }
 }
