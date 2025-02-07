caaf82bbe1b0f2a52ec87c2daa5741e61006a00e
jmai136 <jude.mai@student.centralia.edu>
2025-02-07 07:00:08 -0800

    fix: renamed admin form aspects to be less confusing

--- GourmetShopSolution/GourmetShop.AdminForm/frmAdminMain.Designer.cs
+++ GourmetShopSolution/GourmetShop.AdminForm/frmAdminMain.Designer.cs
@@ -54,7 +54,8 @@
             this.suppliersToolStripMenuItem});
             this.msGourmetShop.Location = new System.Drawing.Point(0, 0);
             this.msGourmetShop.Name = "msGourmetShop";
-            this.msGourmetShop.Size = new System.Drawing.Size(1067, 30);
+            this.msGourmetShop.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.msGourmetShop.Size = new System.Drawing.Size(800, 24);
             this.msGourmetShop.TabIndex = 0;
             this.msGourmetShop.Text = "Gourmet Shop Menu Strip";
             // 
@@ -63,13 +64,13 @@
             this.miFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.exitToolStripMenuItem});
             this.miFile.Name = "miFile";
-            this.miFile.Size = new System.Drawing.Size(46, 24);
+            this.miFile.Size = new System.Drawing.Size(37, 20);
             this.miFile.Text = "File";
             // 
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(116, 26);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(93, 22);
             this.exitToolStripMenuItem.Text = "Exit";
             this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
@@ -78,13 +79,13 @@
             this.productsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.vts_mi_ViewProducts});
             this.productsToolStripMenuItem.Name = "productsToolStripMenuItem";
-            this.productsToolStripMenuItem.Size = new System.Drawing.Size(80, 24);
+            this.productsToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
             this.productsToolStripMenuItem.Text = "Products";
             // 
             // vts_mi_ViewProducts
             // 
             this.vts_mi_ViewProducts.Name = "vts_mi_ViewProducts";
-            this.vts_mi_ViewProducts.Size = new System.Drawing.Size(124, 26);
+            this.vts_mi_ViewProducts.Size = new System.Drawing.Size(99, 22);
             this.vts_mi_ViewProducts.Text = "View";
             this.vts_mi_ViewProducts.Click += new System.EventHandler(this.vts_mi_ViewProducts_Click);
             // 
@@ -93,43 +94,44 @@
             this.suppliersToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.vts_mi_ViewSuppliers});
             this.suppliersToolStripMenuItem.Name = "suppliersToolStripMenuItem";
-            this.suppliersToolStripMenuItem.Size = new System.Drawing.Size(84, 24);
+            this.suppliersToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
             this.suppliersToolStripMenuItem.Text = "Suppliers";
             // 
             // vts_mi_ViewSuppliers
             // 
             this.vts_mi_ViewSuppliers.Name = "vts_mi_ViewSuppliers";
-            this.vts_mi_ViewSuppliers.Size = new System.Drawing.Size(124, 26);
+            this.vts_mi_ViewSuppliers.Size = new System.Drawing.Size(99, 22);
             this.vts_mi_ViewSuppliers.Text = "View";
             this.vts_mi_ViewSuppliers.Click += new System.EventHandler(this.vts_mi_ViewSuppliers_Click);
             // 
             // lblInstructions
             // 
             this.lblInstructions.AutoSize = true;
-            this.lblInstructions.Location = new System.Drawing.Point(246, 9);
-            this.lblInstructions.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblInstructions.Location = new System.Drawing.Point(184, 7);
             this.lblInstructions.Name = "lblInstructions";
-            this.lblInstructions.Size = new System.Drawing.Size(377, 16);
+            this.lblInstructions.Size = new System.Drawing.Size(301, 13);
             this.lblInstructions.TabIndex = 1;
             this.lblInstructions.Text = "Navigate to either products or suppliers via the above toolstrip.";
             // 
             // dgvAvailableProducts
             // 
             this.dgvAvailableProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvAvailableProducts.Location = new System.Drawing.Point(39, 106);
+            this.dgvAvailableProducts.Location = new System.Drawing.Point(29, 86);
+            this.dgvAvailableProducts.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dgvAvailableProducts.Name = "dgvAvailableProducts";
             this.dgvAvailableProducts.RowHeadersWidth = 51;
             this.dgvAvailableProducts.RowTemplate.Height = 24;
-            this.dgvAvailableProducts.Size = new System.Drawing.Size(394, 385);
+            this.dgvAvailableProducts.Size = new System.Drawing.Size(296, 313);
             this.dgvAvailableProducts.TabIndex = 2;
             this.dgvAvailableProducts.SelectionChanged += new System.EventHandler(this.dgvAvailableProducts_SelectionChanged);
             // 
             // lblSalesInstructions
             // 
             this.lblSalesInstructions.AutoSize = true;
-            this.lblSalesInstructions.Location = new System.Drawing.Point(74, 516);
+            this.lblSalesInstructions.Location = new System.Drawing.Point(56, 419);
+            this.lblSalesInstructions.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblSalesInstructions.Name = "lblSalesInstructions";
-            this.lblSalesInstructions.Size = new System.Drawing.Size(261, 16);
+            this.lblSalesInstructions.Size = new System.Drawing.Size(211, 13);
             this.lblSalesInstructions.TabIndex = 3;
             this.lblSalesInstructions.Text = "Click on a product to view sales information";
             // 
@@ -137,19 +139,21 @@
             // 
             this.lblHeader.AutoSize = true;
             this.lblHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblHeader.Location = new System.Drawing.Point(299, 49);
+            this.lblHeader.Location = new System.Drawing.Point(224, 40);
+            this.lblHeader.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblHeader.Name = "lblHeader";
-            this.lblHeader.Size = new System.Drawing.Size(488, 32);
+            this.lblHeader.Size = new System.Drawing.Size(403, 26);
             this.lblHeader.TabIndex = 4;
-            this.lblHeader.Text = "Sales Information: Filtered by Product";
+            this.lblHeader.Text = "Sales Information of Continued Products";
             // 
             // lblTotalUnitsSold
             // 
             this.lblTotalUnitsSold.AutoSize = true;
             this.lblTotalUnitsSold.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTotalUnitsSold.Location = new System.Drawing.Point(493, 177);
+            this.lblTotalUnitsSold.Location = new System.Drawing.Point(370, 144);
+            this.lblTotalUnitsSold.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblTotalUnitsSold.Name = "lblTotalUnitsSold";
-            this.lblTotalUnitsSold.Size = new System.Drawing.Size(238, 25);
+            this.lblTotalUnitsSold.Size = new System.Drawing.Size(192, 20);
             this.lblTotalUnitsSold.TabIndex = 5;
             this.lblTotalUnitsSold.Text = "Products Total Units Sold:";
             // 
@@ -157,17 +161,18 @@
             // 
             this.lblTotalSalesAmount.AutoSize = true;
             this.lblTotalSalesAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTotalSalesAmount.Location = new System.Drawing.Point(493, 274);
+            this.lblTotalSalesAmount.Location = new System.Drawing.Point(370, 223);
+            this.lblTotalSalesAmount.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblTotalSalesAmount.Name = "lblTotalSalesAmount";
-            this.lblTotalSalesAmount.Size = new System.Drawing.Size(272, 25);
+            this.lblTotalSalesAmount.Size = new System.Drawing.Size(219, 20);
             this.lblTotalSalesAmount.TabIndex = 6;
             this.lblTotalSalesAmount.Text = "Products Total Sales Amount:";
             // 
-            // MainForm
+            // frmAdminMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.lblTotalSalesAmount);
             this.Controls.Add(this.lblTotalUnitsSold);
             this.Controls.Add(this.lblHeader);
@@ -176,9 +181,8 @@
             this.Controls.Add(this.lblInstructions);
             this.Controls.Add(this.msGourmetShop);
             this.MainMenuStrip = this.msGourmetShop;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.Name = "MainForm";
-            this.Text = "Gourmet Shop Main Form";
+            this.Name = "frmAdminMain";
+            this.Text = "Admin";
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.msGourmetShop.ResumeLayout(false);
             this.msGourmetShop.PerformLayout();
